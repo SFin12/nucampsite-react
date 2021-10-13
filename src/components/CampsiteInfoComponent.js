3fbaff70e818e54942bae4108e1044b9bed81859
Sean Finegan <sean.j.finegan@gmail.com>
2021-10-12 21:10:55 -0700

    Header Footer

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+    Card,
+    CardImg,
+    CardImgOverlay,
+    CardText,
+    CardBody,
+    CardTitle,
+} from "reactstrap";
+
+function RenderCampsite({ campsite }) {
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+function renderComments({ comments }) {
+    if (comments) {
+        return (
+            <div className="col-md-5 m-1">
+                <h4>"comments"</h4>
+                {comments.map((comment) => {
+                    return (
+                        <div key={comments.id}>
+                            <div>{comment.text}</div>
+                            <p>
+                                --{comment.author},{" "}
+                                {new Intl.DateTimeFormat("en-US", {
+                                    year: "numeric",
+                                    month: "short",
+                                    day: "2-digit",
+                                }).format(new Date(Date.parse(comment.date)))}
+                            </p>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+    return <div></div>;
+}
+
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <renderComments comments={props.campsite.comments} />
+                </div>
+            </div>
+        );
+    } else {
+        return <div></div>;
+    }
+}
+
+export default CampsiteInfo;
